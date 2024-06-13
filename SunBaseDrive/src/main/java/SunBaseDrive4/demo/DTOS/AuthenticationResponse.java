3558c4cbf6f8c4d36b557241c1094570465c43fb
Rishabh Singh <52191143+rishabh0085@users.noreply.github.com>
2024-06-13 09:51:24 +0530

    Add files via upload

---
+++ SunBaseDrive/src/main/java/SunBaseDrive4/demo/DTOS/AuthenticationResponse.java
@@ -0,0 +1,13 @@
+package SunBaseDrive4.demo.DTOS;
+
+public class AuthenticationResponse {
+    private final String jwt;
+
+    public AuthenticationResponse(String jwt) {
+        this.jwt = jwt;
+    }
+
+    public String getJwt() {
+        return jwt;
+    }
+}
