21d5eafd58b6b6b9d53d17768d69c18f01247ca5
Rishabh Singh <52191143+rishabh0085@users.noreply.github.com>
2024-06-13 09:52:40 +0530

    Delete SunBaseDrive directory

--- SunBaseDrive/src/main/java/SunBaseDrive4/demo/Repository/customerRepository.java
+++
@@ -1,7 +0,0 @@
-package SunBaseDrive4.demo.Repository;
-
-import SunBaseDrive4.demo.Entities.Customer;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface customerRepository extends JpaRepository<Customer, Long> {
-}
